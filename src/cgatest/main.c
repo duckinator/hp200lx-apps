93a3f60d547926ef2822a59dddcb3fcbeb3f1305
Ellen Marie Dash <me@duckie.co>
2020-12-19 01:47:51 -0500

    ehhh?

--- src/cgatest/main.c
+++ src/cgatest/main.c
@@ -1,13 +1,35 @@
+#include <dos.h>
 #include <i86.h>
 
-int main() {
-    union REGS r;
+unsigned char *VIDEO = (char*)0xB8000;
+
+void set_video_mode() {
+    union REGS inregs;
+    union REGS outregs;
 
     /* ah = 00h, int 16h "read key stroke" */
     // http://www.wagemakers.be/english/doc/vga
-    r.h.ah = 0x0; // change screen resolution
-    r.h.al = 0x6; // 640x200
-    int86(0x13, &r, &r);
+    inregs.h.ah = 0x0; // change screen resolution
+    inregs.h.al = 0x6; // 640x200
+    int86(0x10, &inregs, &outregs);
+}
+
+void plot_pixel(int x, int y, unsigned char color) {
+    VIDEO[(y * 640) + x] = color;
+}
+
+int main() {
+    int x, y;
+
+    set_video_mode();
+
+    for (x = 0; x < 256; x++) {
+        for (y = 0; y < 50; y++) {
+            VIDEO[640 + 50 + y] = x;
+        }
+    }
+
+    sleep(3);
 
     return 0;
 }
